61c4c5a751d3c936914f273c137137e6b5325dc3
Fabien Chareun <cacrfabfr@gmail.com>
2022-05-09 14:10:00 +0200

    added middlewares

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+
+import authRoute from './routes/auth.js'
+import usersRoute from './routes/users.js'
+import hotelsRoute from './routes/hotels.js'
+import roomsRoute from './routes/rooms.js'
+
 const app = express()
 dotenv.config()
 
@@ -9,18 +15,17 @@ const PORT = process.env.PORT || 8800
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO)
-    console.log('connected to mongoDB');
+    console.log('connected to mongoDB')
   } catch (error) {
     throw error
   }
 }
 
-mongoose.connection.on('disconnect', () => console.log('mongoDB disconnected'))
-mongoose.connection.on('connected', () => console.log('mongoDB connected'))
-
-app.get('/', (req, res) => {
-  res.send('first request')
-})
+// middlewares
+app.use('/api/auth', authRoute)
+app.use('/api/users', authRoute)
+app.use('/api/hotels', authRoute)
+app.use('/api/rooms', authRoute)
 
 app.listen(8800, () => {
   connect()
