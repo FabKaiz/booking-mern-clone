45be4ec9e9847b104cc8d3504f56b95cb8e4d2cd
Fabien Chareun <cacrfabfr@gmail.com>
2022-05-09 13:43:26 +0200

    express init

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import express from 'express';
+import express from 'express'
+const app = express()
+
+const PORT = process.env.PORT || 8800
+
+app.listen(8800, () => {
+  console.log(`Application listening on port ${PORT}`)
+})
