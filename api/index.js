aef1210761c4af6228b6538c9a727823d1e07643
Fabien Chareun <cacrfabfr@gmail.com>
2022-05-11 11:46:57 +0200

    format

--- api/index.js
+++ api/index.js
@@ -31,12 +31,12 @@ app.use('/api/rooms', roomsRoute)
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500
-  const errorMessage = err.message || "Something went wrong"
+  const errorMessage = err.message || 'Something went wrong'
   return res.status(errorStatus).json({
     success: false,
     status: errorStatus,
     message: errorMessage,
-    stack: err.stack 
+    stack: err.stack,
   })
 })
 
@@ -44,4 +44,3 @@ app.listen(8800, () => {
   connect()
   console.log(`Application listening on port ${PORT}`)
 })
-
