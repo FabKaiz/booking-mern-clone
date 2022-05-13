57a5bc5bc05b53025a657af495d1eab75097b08d
Fabien Chareun <cacrfabfr@gmail.com>
2022-05-13 15:52:11 +0200

    format

--- client/src/context/SearchContext.js
+++ client/src/context/SearchContext.js
@@ -1,6 +1,5 @@
 import { createContext, useReducer } from 'react'
 
-
 const INITIAL_STATE = {
   city: undefined,
   dates: [],
@@ -17,18 +16,25 @@ const SearchReducer = (state, action) => {
   switch (action.type) {
     case 'NEW_SEARCH':
       return action.payload
-    case "RESET_SEARCH":
+    case 'RESET_SEARCH':
       return INITIAL_STATE
     default:
       return state
   }
 }
 
-export const SearchContextProvider = ({children}) => {
+export const SearchContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(SearchReducer, INITIAL_STATE)
 
   return (
-    <SearchContext.Provider value={{ city: state.city, dates: state.dates, options: state.options, dispatch }}>
+    <SearchContext.Provider
+      value={{
+        city: state.city,
+        dates: state.dates,
+        options: state.options,
+        dispatch,
+      }}
+    >
       {children}
     </SearchContext.Provider>
   )
