5add689ec341b906da1f082afb1ac13b67562a20
Fabien Chareun <cacrfabfr@gmail.com>
2022-05-17 14:50:50 +0200

    Navbar  setup and scss

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import './home.scss'
 import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homeContainer">contianer</div>
+      <div className="homeContainer">
+        <Navbar />
+        HomeContainer
+      </div>
     </div>
   )
 }
