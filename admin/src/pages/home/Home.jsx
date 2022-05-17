4024797301c2072f2c4db69341ca5fb89f298c5b
Fabien Chareun <cacrfabfr@gmail.com>
2022-05-17 14:23:50 +0200

    Home setup

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,13 +1,11 @@
 import './home.scss'
 import Sidebar from '../../components/sidebar/Sidebar'
-import Navbar from '../../components/navbar/Navbar'
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <Navbar />
-      <h2>Home</h2>
+      <div className="homeContainer">contianer</div>
     </div>
   )
 }
