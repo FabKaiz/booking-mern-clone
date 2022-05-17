65e69c4cdab9f71c5802f281d6a88e95883ddba5
Fabien Chareun <cacrfabfr@gmail.com>
2022-05-17 14:50:25 +0200

    format

--- admin/src/App.js
+++ admin/src/App.js
@@ -8,23 +8,23 @@ import New from './pages/new/New'
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/">
-        <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="users" >
-          <Route index element={<List />} />
-          <Route path=":userId" element={<Single />} />
-          <Route path="new" element={<New />} />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+          <Route path="products">
+            <Route index element={<List />} />
+            <Route path=":productId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
         </Route>
-        <Route path="products" >
-          <Route index element={<List />} />
-          <Route path=":productId" element={<Single />} />
-          <Route path="new" element={<New />} />
-        </Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
