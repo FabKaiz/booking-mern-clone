9bc929e50db4166e4974d7d60aa6c6d6a2318423
Fabien Chareun <cacrfabfr@gmail.com>
2022-05-23 12:11:04 +0200

    added fetch prop

--- admin/src/components/datatable/Datatable.jsx
+++ admin/src/components/datatable/Datatable.jsx
@@ -1,21 +1,37 @@
 import './datatable.scss'
 import { DataGrid } from '@mui/x-data-grid'
 import { userColumns, userRows } from '../../datatablesource'
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import useFetch from '../../hooks/useFetch.js'
+import axios from 'axios'
 
-const Datatable = () => {
-  const [data, setdata] = useState(userRows)
+const Datatable = ({ columns }) => {
+  const location = useLocation()
+  const path = location.pathname.split('/')[1]
 
-  const handleDelete = (id) => {
-    setdata(data.filter((item) => item.id !== id))
+  const [list, setList] = useState([])
+  const { data, loading, error } = useFetch(`/${path}`)
+
+  useEffect(() => {
+    setList(data)
+  }, [data])
+  
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`/${path}/${id}`)
+      setList(list.filter((item) => item._id !== id))
+    } catch (err) {
+      
+    }
   }
 
   const actionColumn = [
     {
       field: 'action',
       headerName: 'Action',
-      width: 200,
+      width: 140,
       renderCell: (params) => {
         return (
           <div className="cellAction">
@@ -24,7 +40,7 @@ const Datatable = () => {
             </Link>
             <div
               className="deleteButton"
-              onClick={() => handleDelete(params.row.id)}
+              onClick={() => handleDelete(params.row._id)}
             >
               Delete
             </div>
@@ -43,12 +59,13 @@ const Datatable = () => {
         </Link>
       </div>
       <DataGrid
-        rows={data}
-        columns={userColumns.concat(actionColumn)}
+        rows={list}
+        columns={columns.concat(actionColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
         checkboxSelection
         className="datagrid"
+        getRowId={(row) => row._id}
       />
     </div>
   )
