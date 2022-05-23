29982dd2d43a5785744330f1cedf45e6a314c43f
Fabien Chareun <cacrfabfr@gmail.com>
2022-05-23 12:36:43 +0200

    add new users

--- admin/src/App.js
+++ admin/src/App.js
@@ -9,6 +9,7 @@ import './style/dark.scss'
 import { useContext } from 'react'
 import { DarkModeContext } from './context/darkModeContext'
 import { AuthContext } from './context/AuthContext'
+import { hotelColumns, userColumns } from './datatablesource'
 
 function App() {
   const { darkMode } = useContext(DarkModeContext)
@@ -42,7 +43,7 @@ function App() {
                 index
                 element={
                   <ProtectedRoute>
-                    <List />
+                    <List columns={userColumns} />
                   </ProtectedRoute>
                 }
               />
@@ -57,18 +58,18 @@ function App() {
               <Route
                 path="new"
                 element={
-                  <protectedRoute>
+                  <ProtectedRoute>
                     <New inputs={userInputs} title="Add New User" />
-                  </protectedRoute>
+                  </ProtectedRoute>
                 }
               />
             </Route>
-            <Route path="products">
+            <Route path="hotels">
               <Route
                 index
                 element={
                   <ProtectedRoute>
-                    <List />
+                    <List columns={hotelColumns} />
                   </ProtectedRoute>
                 }
               />
@@ -83,9 +84,9 @@ function App() {
               <Route
                 path="new"
                 element={
-                  <protectedRoute>
+                  <ProtectedRoute>
                     <New inputs={productInputs} title="Add New User" />
-                  </protectedRoute>
+                  </ProtectedRoute>
                 }
               />
             </Route>
